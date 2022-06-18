38401ed34c7cf913c8f0c084861f7d668c2bf8fd
Ahlam <lulu@test.com>
2022-06-18 22:47:00 +0300

    new method

--- JavaScript/example.js
+++ JavaScript/example.js
@@ -6,3 +6,16 @@ function printMax(array){
     }
     console.log(max);
 }
+
+
+// 
+function printMin(array){
+    let min= 0;
+    for(let i =0; i<array.length;i++){
+        if(array[i]< max)
+        max = array[i];
+    }
+    console.log(min);
+}
+
+
